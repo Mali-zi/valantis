cb89a565dd4abb7e3d893b1f52ba2988d2450745
Natali Zikrach <mne310872@yandex.ru>
2024-03-02 23:08:38 +0300

    fix: fix tsconfig.json file

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,16 @@ import { store, persistor } from './redux/app/store.ts';
 import './index.scss';
 import { PersistGate } from 'redux-persist/integration/react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
