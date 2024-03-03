ab962fa1948d53202b4eb21e8feff766791fb40c
Natali Zikrach <mne310872@yandex.ru>
2024-03-03 16:34:14 +0300

    fix: change config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import postcss from './postcss.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://mali-zi.github.io/valantis/',
   plugins: [react()],
   css: {
     postcss,
